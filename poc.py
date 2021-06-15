cbe777d02c10fa7d102116ba7911f699ab9760fb
vishwas-katiyar <vishwaskatiyar@users.noreply.github.com>
2021-06-15 18:21:11 +0530

    'lool'

--- poc.py
+++ poc.py
@@ -7,6 +7,7 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 
-browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+# browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
-browser.get('https://web.whatsapp.com/')
+# browser.get('https://web.whatsapp.com/')
+print('looooooo'*30)
